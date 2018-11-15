2f74d2fe87cca94c81f69f1174c49426f0e80b6d
taojiaqi <124918146@qq.com>
2018-11-15 14:01:04 +0800

    初版提交

---
+++ src/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="greeting">The environment is ready!</text>
+    <div>
+       <p v-if="seen">现在你可以看到我了</p>
+    </div>
+
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">逆转消息</button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+      seen: false,
+      message: 'Hello Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
